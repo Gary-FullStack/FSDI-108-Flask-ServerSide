af34257daf3e1a2b94308213573f3f6919c4f0db
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-02-13 15:48:10 -0500

    FSDI FINAL

--- server.py
+++ server.py
@@ -77,17 +77,40 @@ def search_product(text):
     return json.dumps(results)
 
 
-# gave this a try, but it didn't work.
-
-@app.get("/api/categories/<title>")
-def checkKey(dic, key):
-    for key, value in mock_catalog():
-        return key
-    else:
-        return abort(404, " we no longer have any of those items")
-        results.append(key)
+@app.get("/api/categories")
+def get_categories():
+    results = []
+    for prod in mock_catalog:
+        cat = prod["category"]
+        if cat not in results:
+            results.append(cat)
 
     return json.dumps(results)
 
 
+@app.get("/api/total")
+def get_total():
+    total = 0
+    for prod in mock_catalog:
+        total += prod["price"]
+
+    return json.dumps(total)
+
+
+@app.get("/api/lower/<price>")
+def get_lower(price):
+    price = float(price)
+    results = []
+    for prod in mock_catalog:
+        if prod["price"] <= price:
+            results.append(prod)
+    return json.dumps(results)
+
+
+    # challenge,  find and return least costly items
+# create a cheapest = mock_catalog[0]
+# for loop to travel the list
+# get every prod from the list
+# if the price of prod is lower than the price of cheapest
+# then update cheapest to be the prod (cheapest = prod)
 app.run(debug=True)
